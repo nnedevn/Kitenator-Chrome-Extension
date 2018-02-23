9d99f7da2ad8ec0271e34f89a991d86f1b022e3b
Nick Nedev <nedevn@gmail.com>
2018-02-23 08:13:37 -0800

    Initial Commit

---
+++ content.js
@@ -0,0 +1,25 @@
+catUrlArray = [
+
+]
+
+let pageImages = document.getElementsByTagName('img');
+
+// function getCat(){
+//   $.ajax({
+//     method: "GET",
+//     url: "https://cataas.com/cat"
+//   }).done(function(cat){
+//     return cat;
+//   })
+// }
+
+
+// for (imgEl of pageImages){
+//   console.log(imgEl.src);
+//   // let rand = Math.floor(Math.random() * catUrlArray.length);
+//   // let catSrc
+//   // let fileUrl 
+
+//    imgEl.src = "https://cataas.com/c"
+// }
+// console.log(getCat());
